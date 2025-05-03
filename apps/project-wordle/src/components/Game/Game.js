338878671bf554bc7986c08fcdc99897207cbc6e
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2025-05-03 14:38:09 -0700

    Keep track of Wordle guesses

--- apps/project-wordle/src/components/Game/Game.js
+++ apps/project-wordle/src/components/Game/Game.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-
+import Guess from '../Guess/Guess';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,31 +10,44 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = useState([]);
   const [guessInput, setGuessInput] = useState('');
 
   return (
-    <form
-      className="guess-input-wrapper"
-      onSubmit={(event) => {
-        event.preventDefault();
+    <div>
+      <Guess guesses={guesses} />
+      <form
+        className="guess-input-wrapper"
+        onSubmit={(event) => {
+          event.preventDefault();
 
-        console.log(guessInput);
-        setGuessInput('');
-      }}
-    >
-      <label htmlFor="guess-input">Enter guess:</label>
-      <input
-        id="guess-input"
-        name="guess-input"
-        type="text"
-        minLength={5}
-        maxLength={5}
-        value={guessInput}
-        onChange={(event) =>
-          setGuessInput(event.target.value.toUpperCase())
-        }
-      />
-    </form>
+          if (guesses.length === NUM_OF_GUESSES_ALLOWED) {
+            return;
+          }
+
+          const newGuesses = [...guesses];
+
+          newGuesses[guesses.length] = guessInput;
+
+          setGuesses(newGuesses);
+          setGuessInput('');
+        }}
+      >
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          name="guess-input"
+          type="text"
+          minLength={5}
+          maxLength={5}
+          disabled={guesses.length === NUM_OF_GUESSES_ALLOWED}
+          value={guessInput}
+          onChange={(event) =>
+            setGuessInput(event.target.value.toUpperCase())
+          }
+        />
+      </form>
+    </div>
   );
 }
 
