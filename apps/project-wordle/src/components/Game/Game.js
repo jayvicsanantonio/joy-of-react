9f8888696a008ecc25b423ef9c6ffea6835177d1
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2025-05-03 13:38:40 -0700

    Add Wordle guess input

--- apps/project-wordle/src/components/Game/Game.js
+++ apps/project-wordle/src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
@@ -9,7 +9,32 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guessInput, setGuessInput] = useState('');
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        console.log(guessInput);
+        setGuessInput('');
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        name="guess-input"
+        type="text"
+        minLength={5}
+        maxLength={5}
+        value={guessInput}
+        onChange={(event) =>
+          setGuessInput(event.target.value.toUpperCase())
+        }
+      />
+    </form>
+  );
 }
 
 export default Game;
